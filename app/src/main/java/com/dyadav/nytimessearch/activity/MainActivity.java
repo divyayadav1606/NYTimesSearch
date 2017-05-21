83705e4eab0da40009d012d5bf927bd2f8b0fe8a
Divya Yadav <divyayadav1606@gmail.com>
2017-05-20 23:34:34 -0700

    Code cleanup
    remove redundant code

--- app/src/main/java/com/dyadav/nytimessearch/activity/MainActivity.java
+++ app/src/main/java/com/dyadav/nytimessearch/activity/MainActivity.java
@@ -1,7 +1,6 @@
 package com.dyadav.nytimessearch.activity;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 
@@ -9,9 +8,7 @@ import com.dyadav.nytimessearch.R;
 import com.dyadav.nytimessearch.fragment.NewsFragment;
 
 public class MainActivity extends AppCompatActivity {
-
-    Fragment selectedFragment = null;
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,12 +19,5 @@ public class MainActivity extends AppCompatActivity {
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.add(R.id.holder_layout, NewsFragment.newInstance());
         transaction.commit();
-    }
-
-    public void onSaveInstanceState(Bundle outState){
-        //getFragmentManager().putFragment(outState, "fragment", selectedFragment);
-    }
-    public void onRestoreInstanceState(Bundle inState){
-        //myFragment = getFragmentManager().getFragment(inState,"myfragment");
     }
 }
